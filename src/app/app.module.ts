341a7999555dd0288cb5e6111a0bdd1674e1842d
PratikshaLanet <lanetteam.pratiksha@gmail.com>
2018-08-10 15:46:31 +0530

    Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {UserService} from "./user/user.service";
+import { ReversePipe } from './shared/reverse.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ReversePipe
   ],
   imports: [
     BrowserModule
